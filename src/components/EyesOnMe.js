3729e5e2614d3fe7c15ab67f74a3f8c65578f335
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-09-18 15:50:51 +0100

    jo

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+
+        focuseResp () {
+            console.log('Good!')
+        }
+
+        blurResp () {
+            console.log('Hey! Eyes on me!')
+        }
+    render (){
+     return (<button onFocus={this.focuseResp} onBlur={this.blurResp}>Some Button</button>)
+    }
+
+}
